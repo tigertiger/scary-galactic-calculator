0d8a5d67a5afbd9046fc62844bccaf1f98d50864
Melissa <melissa.scmi@gmail.com>
2021-09-03 15:04:05 -0700

    backtrack on tests; T7 pass: read life expect from user input

--- src/js/gcalc.js
+++ src/js/gcalc.js
@@ -1,21 +1,61 @@
 export class Ager {
-  constructor(age, homeland, assignedSex, motivation) {
+  constructor(age, homeland, assignedSex, motivation, lifeExpect) {
     this.age = age;
     this.homeland = homeland;
     this.assignedSex = assignedSex;
     this.motivation = motivation;
-    this.lifeExpect = 0;
+    this.lifeExpect = lifeExpect;
   }
 
-  baseLifeExpect() {
-    let modifier = 0;
-    if (this.assignedSex === "Female") {
-      modifier += 65;
-      this.lifeExpect += modifier;
-    } else {
-      modifier += 62;
-      this.lifeExpect += modifier;
-    }
-    return this.lifeExpect;
-  }
+
+  // The other way...
+
+  // export class Ager {
+  //   constructor(age, homeland, assignedSex, motivation) {
+  //     this.age = age;
+  //     this.homeland = homeland;
+  //     this.assignedSex = assignedSex;
+  //     this.motivation = motivation;
+  //     this.lifeExpect = 0;
+  //   }
+  // baseLifeExpect() {
+  //   let modifier = 0;
+  //   if (this.assignedSex === "Female" && this.homeland === "North America") {
+  //     modifier += 81;
+  //     this.lifeExpect += modifier;
+  //   } else if (this.assignedSex === "Female" && this.homeland === "Oceania") {
+  //     modifier += 81;
+  //     this.lifeExpect += modifier;
+  //   } else if (this.assignedSex === "Female" && this.homeland === "Europe") {
+  //     modifier += 82;
+  //     this.lifeExpect += modifier;
+  //   } else if (this.assignedSex === "Female" && this.homeland === "Latin America") {
+  //     modifier += 79;
+  //     this.lifeExpect += modifier;
+  //   } else if (this.assignedSex === "Female" && this.homeland === "Asia") {
+  //     modifier += 75;
+  //     this.lifeExpect += modifier;
+  //   } else if (this.assignedSex === "Female" && this.homeland === "Africa") {
+  //     modifier += 65;
+  //     this.lifeExpect += modifier;
+  //   } else if (this.assignedSex === "Male" && this.homeland === "North America") {
+  //     modifier += 77;
+  //     this.lifeExpect += modifier;
+  //   } else if (this.assignedSex === "Male" && this.homeland === "Oceania") {
+  //     modifier += 77;
+  //     this.lifeExpect += modifier;
+  //   } else if (this.assignedSex === "Male" && this.homeland === "Europe") {
+  //     modifier += 75;
+  //     this.lifeExpect += modifier;
+  //   } else if (this.assignedSex === "Male" && this.homeland === "Latin America") {
+  //     modifier += 72;
+  //     this.lifeExpect += modifier;
+  //   } else if (this.assignedSex === "Male" && this.homeland === "Asia") {
+  //     modifier += 71;
+  //     this.lifeExpect += modifier;
+  //   } else {
+  //     modifier += 62;
+  //   }
+  //   return this.lifeExpect;
+  // }
 }
